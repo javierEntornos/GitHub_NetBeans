9ade9d60211ceb96fca48efc0d903a7fee3f89d3
javierEntornos <javier.cuagar@educa.jcyl.es>
2023-12-18 20:48:26 +0100

    Clonado el repositorio

---
+++ GitHubProject/src/main/java/com/mycompany/githubproject/GitHubProject.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubproject;
+
+/**
+ *
+ * @author JAVIERCUADRADOGARCIA
+ */
+public class GitHubProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde NetBeans!");
+        
+        
+        
+    }
+}
